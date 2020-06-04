2f4622ed20c91c47ac4f1a54aa05c8038cb13e45
Afshin <47743758+AKaramifar@users.noreply.github.com>
2020-06-04 09:52:41 +0100

    search bar design

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return(
         <div id="Div_Header_JSX" className="Div_Header_CSS">
             <p id="P_TitleQuestion_JSX" className="P_TitleQuestion_CSS">Where in the world?</p>
-            <p id="P_ThemeSwitcher_JSX" class="P_ThemeSwitcher_CSS"><i class="far fa-sun" aria-hidden="true"></i> Light Mode</p>
+            <p id="P_ThemeSwitcher_JSX" className="P_ThemeSwitcher_CSS"><i className="I_Sun_CSS far fa-sun" aria-hidden="true"></i> Light Mode</p>
 
         </div>
     );
--- src/components/ShowAllCountries.js
+++ src/components/ShowAllCountries.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import "../components/ShowAllCountries.css";
 
 const ShowAllCountries = () => {
-    return(
-        <div id="Div_ShowAllCountries">
+  return (
+    <div id="Div_ShowAllCountries_JSX" className="Div_ShowAllCountries_CSS">
+      <div id="Div_SearchBar_JSX" className="Div_SearchBar_CSS">
+        <div id="Div_SearchCountries_JSX" className="Div_SearchCountries_CSS">
+          <i id="I_SearchIcon_JSX" className="I_SearchIcon_CSS fas fa-search" aria-hidden="true"></i>
+          <input id="Input_Search_JSX" className="Input_Search_CSS" placeholder="Search for a country . . ."/>
+        </div>
+        <div id="Div_CountriesRegion_JSX" className="Div_CountriesRegion_CSS">
+            <p id="P_SelectedRegion_JSX" className="P_SelectedRegion_CSS">All Region &nbsp;&nbsp;
+                <i className="I_DownSymbol_CSS fas fa-sort-down" aria-hidden="true"></i>
+            </p>
 
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default ShowAllCountries;
