242db72b552e914cae584c6b87a88b81e72c94be
Afshin <47743758+AKaramifar@users.noreply.github.com>
2020-06-05 13:58:45 +0100

    search bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import "./App.css";
-import Header from './components/Header.js';
-import ShowAllCountries from './components/ShowAllCountries.js';
+import Header from "./components/Header.js";
+import ShowAllCountries from "./components/ShowAllCountries.js";
 
 function App() {
+  const[countriesInfo, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.eu/rest/v2/all")
+    .then((res) => {
+      if (res.status >= 400) {
+        throw new Error("Bad response from server");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      setData(data);
+      console.log(data)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  },[]);
   return (
     <div id="Div_App_JSX" className="Div_App_CSS">
       <Header />
       <div id="Div_Main_JSX" className="Div_Main_CSS">
-        <ShowAllCountries />
+        <ShowAllCountries data={countriesInfo}/>
         {/* <FullViewOfCountry /> */}
       </div>
     </div>
--- src/components/ShowAllCountries.js
+++ src/components/ShowAllCountries.js
@@ -1,7 +1,20 @@
 import React from "react";
 import "../components/ShowAllCountries.css";
 
-const ShowAllCountries = () => {
+const ShowAllCountries = (props) => {
+  let region = [];
+  let regionIndex = [];
+  let regionVisiblityState =  false;
+  const selectBoxState = () => {
+    regionVisiblityState ? regionVisiblityState = false : regionVisiblityState = true;
+    document.getElementById("P_Region_JSX").style.display = !regionVisiblityState  ? "none" : "flex";
+    regionIndex.forEach((region) => {
+      document.getElementById("P_Region_" + region + "_JSX").style.display = !regionVisiblityState  ? "none" : "flex";
+    })
+  }
+  const selectedRegoin = (e) => {
+    document.getElementById("P_SelectedRegion_JSX").textContent = e;
+  }
   return (
     <div id="Div_ShowAllCountries_JSX" className="Div_ShowAllCountries_CSS">
       <div id="Div_SearchBar_JSX" className="Div_SearchBar_CSS">
@@ -9,12 +22,24 @@ const ShowAllCountries = () => {
           <i id="I_SearchIcon_JSX" className="I_SearchIcon_CSS fas fa-search" aria-hidden="true"></i>
           <input id="Input_Search_JSX" className="Input_Search_CSS" placeholder="Search for a country . . ."/>
         </div>
-        <div id="Div_CountriesRegion_JSX" className="Div_CountriesRegion_CSS">
-            <p id="P_SelectedRegion_JSX" className="P_SelectedRegion_CSS">All Region &nbsp;&nbsp;
-                <i className="I_DownSymbol_CSS fas fa-sort-down" aria-hidden="true"></i>
-            </p>
-
+        <div id="Div_CountriesRegion_JSX" className="Div_CountriesRegion_CSS" onClick={selectBoxState}>
+            <p id="P_SelectedRegion_JSX" className="P_SelectedRegion_CSS">All Region</p>            
+            <i className="I_DownSymbol_CSS fas fa-sort-down" aria-hidden="true"></i>
+            <p id={"P_Region_JSX"} className="P_Region_CSS" onClick={(e)=> selectedRegoin(e.target.textContent)}>All Region</p>
+            {
+              props.data.sort((a, b) => a.region.localeCompare(b.region)).map((country, index) => {
+                if(region.includes(country.region) || country.region === "") return null
+                  region.push(country.region);
+                  regionIndex.push(index);
+                  return(
+                    <p id={"P_Region_" + index + "_JSX"} className="P_Region_CSS" key={index} onClick={(e)=> selectedRegoin(e.target.textContent)}>{country.region}</p>
+                  );
+              })
+            }
         </div>
+      </div>
+      <div id="Div_ShowCountries_JSX" className="Div_ShowCountries_CSS">
+
       </div>
     </div>
   );
